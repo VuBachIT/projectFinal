96767b71072e1be1397274cce8ab890493aa72bc
unknown <vhbach0605@gmail.com>
2023-05-23 15:11:22 +0700

    Add function

--- Backend/controllers/customerClass.js
+++ Backend/controllers/customerClass.js
@@ -1,36 +1,36 @@
 let models = require('../models')
 let customer = models.Customer
 
-class Customer{
-    getAll(condition){
-        return new Promise((resolve,reject)=>{
+class Customer {
+    getAll(condition) {
+        return new Promise((resolve, reject) => {
             customer.findAll(condition)
-            .then(response => {
-                let data = []
-                response.forEach(element => {
-                    data.push(element.dataValues)
-                });
-                resolve(data.sort((a,b) => {return a.id - b.id}))
-            })
-            .catch(error =>{reject(new Error(error))})
-        }) 
+                .then(response => {
+                    let data = []
+                    response.forEach(element => {
+                        data.push(element.dataValues)
+                    });
+                    resolve(data.sort((a, b) => { return a.id - b.id }))
+                })
+                .catch(error => { reject(new Error(error)) })
+        })
     }
 
-    getOne(condition){
-        return new Promise((resolve,reject)=>{
+    getOne(condition) {
+        return new Promise((resolve, reject) => {
             customer.findOne(condition)
-            .then(response => {
-                resolve(response != null ? response.dataValues : null)
-            })
-            .catch(error =>{reject(new Error(error))})
-        }) 
+                .then(response => {
+                    resolve(response != null ? response.dataValues : null)
+                })
+                .catch(error => { reject(new Error(error)) })
+        })
     }
 
-    insertData(data){
-        return new Promise((resolve,reject)=>{
+    insertData(data) {
+        return new Promise((resolve, reject) => {
             customer.create(data)
-            .then(resolve(true))
-            .catch(error => reject(new Error(error)))
+                .then(resolve(true))
+                .catch(error => reject(new Error(error)))
         })
     }
 
@@ -44,7 +44,7 @@ class Customer{
         })
     }
 
-    deleteData(data,condition){
+    deleteData(data, condition) {
         return new Promise((resolve, reject) => {
             customer.update(data, condition)
                 .then(resolve(true))
--- Backend/controllers/partnerClass.js
+++ Backend/controllers/partnerClass.js
@@ -1,29 +1,37 @@
 let models = require('../models')
 let partner = models.Partner
 
-class Partner{
-    getAll(condition){
-        return new Promise((resolve,reject)=>{
+class Partner {
+    getAll(condition) {
+        return new Promise((resolve, reject) => {
             partner.findAll(condition)
-            .then(response => {
-                let data = []
-                response.forEach(element => {
-                    data.push(element.dataValues)
-                });
-                resolve(data.sort((a,b) => {return a.id - b.id}))
-            })
-            .catch(error =>{reject(new Error(error))})
-        }) 
+                .then(response => {
+                    let data = []
+                    response.forEach(element => {
+                        data.push(element.dataValues)
+                    });
+                    resolve(data.sort((a, b) => { return a.id - b.id }))
+                })
+                .catch(error => { reject(new Error(error)) })
+        })
     }
 
-    getOne(condition){
-        return new Promise((resolve,reject)=>{
+    getOne(condition) {
+        return new Promise((resolve, reject) => {
             partner.findOne(condition)
-            .then(response => {
-                resolve(response != null ? response.dataValues : null)
-            })
-            .catch(error =>{reject(new Error(error))})
-        }) 
+                .then(response => {
+                    resolve(response != null ? response.dataValues : null)
+                })
+                .catch(error => { reject(new Error(error)) })
+        })
+    }
+
+    insertData(data) {
+        return new Promise((resolve, reject) => {
+            partner.create(data)
+                .then(resolve(true))
+                .catch(error => reject(new Error(error)))
+        })
     }
 
     updateData(data, condition) {
@@ -36,7 +44,7 @@ class Partner{
         })
     }
 
-    deleteData(data,condition){
+    deleteData(data, condition) {
         return new Promise((resolve, reject) => {
             partner.update(data, condition)
                 .then(resolve(true))
